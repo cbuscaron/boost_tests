f3b20c5df83068797df1de1bd1c00333f8a4aa59
camilo <cbuscaron@fetchrobotics.com>
2016-01-13 18:42:47 -0800

    binding arguments to handler

--- timer_test.cpp
+++ timer_test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <boost/array.hpp>
 #include <boost/asio.hpp>
+#include <boost/bind.hpp>
 
 #include<boost/date_time/posix_time/posix_time.hpp>
 
@@ -11,14 +12,36 @@ void print(const boost::system::error_code& )
  std:cout <<"Hello World Asynch" << std::endl;
 }
 
+//pointer to a timer object & counter to stop program
 
-int main(){
-  boost::array<int, 4> arr = {{1,2,3,4}};
-  cout << "hi" << arr[0] << '\n';
+void print(const boost::system::error_code& /*e*/,
+    boost::asio::deadline_timer* t, int* count)
+{
+  if (*count < 5)
+  {
+    std::cout << *count << std::endl;
+    ++(*count);
 
+    t->expires_at(t->expires_at() + boost::posix_time::seconds(1));
+    t->async_wait(boost::bind(print,
+          boost::asio::placeholders::error, t, count));
+  }
+}
 
+int main()
+{
   boost::asio::io_service io;
-  
+
+  int count = 0;
+  boost::asio::deadline_timer t(io, boost::posix_time::seconds(1));
+  t.async_wait(boost::bind(print,
+        boost::asio::placeholders::error, &t, &count));
+
+  io.run();
+
+  std::cout << "Final count is " << count << std::endl;
+
+  /*
   //synchronous timer 
   boost::asio::deadline_timer t(io, boost::posix_time::seconds(5));
   //t.wait();
@@ -28,6 +51,7 @@ int main(){
 
   io.run(); //asio library callbacks handler will only run be called from threads that 
             // currently calling io_service::run()
+  */
 
   return 0;
 }
